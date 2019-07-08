b23545b986235d085ee8c4b020d5ea805bc5b5fa
Jannik Zschiesche <jz@becklyn.com>
2019-07-08 21:21:21 +0200

    Extract voters to just being another visitor

---
+++ src/Visitor/VoterVisitor.php
@@ -0,0 +1,46 @@
+<?php declare(strict_types=1);
+
+namespace Becklyn\Menu\Visitor;
+
+use Becklyn\Menu\Item\MenuItem;
+use Becklyn\Menu\Voter\VoterInterface;
+
+class VoterVisitor implements ItemVisitor
+{
+    /**
+     * @var VoterInterface[]|iterable
+     */
+    private $voters;
+
+
+    /**
+     * @param VoterInterface[]|iterable $voters
+     */
+    public function __construct (iterable $voters)
+    {
+        $this->voters = $voters;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function visit (MenuItem $item) : void
+    {
+        // only apply voters if the item isn't yet marked as "current" from the construction
+        if (!$item->isCurrent())
+        {
+            foreach ($this->voters as $voter)
+            {
+                $current = $voter->vote($item);
+
+                // the first matching voter wins
+                if (null !== $current)
+                {
+                    $item->setCurrent($current);
+                    break;
+                }
+            }
+        }
+    }
+
+}
